5324f973734750e51a47390de090672f4028d165
Fairus Atoir <36189723+fairusatoir@users.noreply.github.com>
2023-05-22 16:57:45 +0700

    feat: logger clean

---
+++ app/Http/Middleware/AddUUID.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AddUUID
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!$request->header('X-Request-ID')) {
+            $request->headers->add(['X-Request-ID' => Str::uuid()]);
+        }
+        return $next($request);
+    }
+}

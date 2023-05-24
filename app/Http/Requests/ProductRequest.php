2022484654c382265a40f07ffe26a4dc1e65e7f7
Fairus Atoir <36189723+fairusatoir@users.noreply.github.com>
2023-05-24 17:57:23 +0700

    fix: sonarcloud code smell

--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -23,7 +23,7 @@ class ProductRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|max:255',
+            'name' => 'required|max:100',
             'slug' => 'required|max:255',
             'type' => 'required|max:255',
             'description' => 'required|max:255',

bef732291d5bedfb32631e772d152bd0fcd2a0e8
Fairus Atoir <36189723+fairusatoir@users.noreply.github.com>
2023-06-10 21:17:59 +0700

    add: unit test API Product

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Helpers\ApiFormatter;
-use App\Http\Controllers\Controller;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Helpers\ApiFormatter;
+use App\Helpers\Stringer;
+use Illuminate\Support\Facades\Log;
+use App\Http\Controllers\Controller;
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class ProductController extends Controller
 {
@@ -14,36 +18,64 @@ class ProductController extends Controller
      */
     public function index(Request $request)
     {
-        $name = $request->input('name');
-        $type = $request->input('type');
-        $priceFrom = $request->input('pricefrom');
-        $priceTo = $request->input('priceto');
-        $orderBy = $request->input('orderby');
-        $limit = $request->input('limit', 10);
+        try {
+            Log::info(
+                "[BEGIN][Get All Product]",
+                [
+                    'headers' => $request->header(),
+                    'body' => $request->all()
+                ]
+            );
 
-        $data = Product::with('galleries')
-            ->when(!$this->is_null($name), function ($query) use ($name) {
-                return $query->where('name', 'like', '%' . $name . '%');
-            })
-            ->when(!$this->is_null($type), function ($query) use ($type) {
-                return $query->where('type', 'like', '%' . $type . '%');
-            })
-            ->when(!$this->is_null($priceFrom), function ($query) use ($priceFrom) {
-                return $query->where('price', '>', $priceFrom);
-            })
-            ->when(!$this->is_null($priceTo), function ($query) use ($priceTo) {
-                return $query->where('price', '<', $priceTo);
-            })
-            ->when(!$this->is_null($orderBy), function ($query) use ($orderBy) {
-                return $query->orderBy($orderBy);
-            }, function ($query) {
-                $query->orderBy('name');
-            })
-            ->paginate($limit);
+            $name = $request->input('name');
+            $type = $request->input('type');
+            $priceFrom = $request->input('pricefrom');
+            $priceTo = $request->input('priceto');
+            $orderBy = $request->input('orderby');
+            $limit = $request->input('limit', 10);
 
-        return response()->json(
-            ApiFormatter::success($data, 'Data Produk Berhasil diambil!')
-        );
+            $data = Product::with('galleries')
+                ->when(!Stringer::is_null($name), function ($query) use ($name) {
+                    return $query->where('name', 'like', '%' . $name . '%');
+                })
+                ->when(!Stringer::is_null($type), function ($query) use ($type) {
+                    return $query->where('type', 'like', '%' . $type . '%');
+                })
+                ->when(!Stringer::is_null($priceFrom), function ($query) use ($priceFrom) {
+                    return $query->where('price', '>', $priceFrom);
+                })
+                ->when(!Stringer::is_null($priceTo), function ($query) use ($priceTo) {
+                    return $query->where('price', '<', $priceTo);
+                })
+                ->when(!Stringer::is_null($orderBy), function ($query) use ($orderBy) {
+                    return $query->orderBy($orderBy);
+                }, function ($query) {
+                    $query->orderBy('name');
+                })
+                ->paginate($limit);
+
+            Log::info(
+                "[END][Get All Product]",
+                [
+                    'response' => $data,
+                ]
+            );
+
+            return response()->json(
+                ApiFormatter::success($data, 'Data Produk Berhasil diambil!')
+            );
+        } catch (Exception $e) {
+            Log::error(
+                "[ERROR][{$e->getMessage()}]",
+                [
+                    "execption" => $e,
+                ],
+            );
+
+            return response()->json(
+                ApiFormatter::error($e->getMessage())
+            );
+        }
     }
 
     /**
@@ -57,9 +89,53 @@ class ProductController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Request $request, string $slug)
     {
-        //
+        try {
+            Log::info(
+                "[BEGIN][Show {$slug}]",
+                [
+                    'headers' => $request->header(),
+                    'body' => $request->all()
+                ]
+            );
+
+            $product = Product::where('slug', $slug)->get();
+            if (!$product) {
+                throw new ModelNotFoundException("Product {$slug} not Found!");
+            }
+
+            Log::info(
+                "[END][Show {$slug}]",
+                [
+                    'response' => $product,
+                ]
+            );
+
+            return response()->json(
+                ApiFormatter::success($product, "Data {$slug} Berhasil diambil!")
+            );
+        } catch (ModelNotFoundException $e) {
+            Log::error(
+                "[NOTFOUND][{$e->getMessage()}]",
+                [
+                    "execption" => $e,
+                ],
+            );
+            return response()->json(
+                ApiFormatter::error($e->getMessage(), 404)
+            );
+        } catch (Exception $e) {
+            Log::error(
+                "[ERROR][{$e->getMessage()}]",
+                [
+                    "execption" => $e,
+                ],
+            );
+            return response()->json(
+                ApiFormatter::error($e->getMessage())
+            );
+        }
     }
 
     /**
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -12,37 +12,4 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
-
-    public function logError(Request $request, Exception $e)
-    {
-        $traceJson = json_encode($e->getTrace());
-        $headers = json_encode($request->header());
-        $body = json_encode($request->all());
-        Log::error([
-            'request-id' => $request->header('x-request-id'),
-            'message' => "[ERROR] " . $e->getMessage(),
-            'file' => $e->getFile(),
-            'line' => $e->getLine(),
-            'header' => $headers,
-            'body' => $body,
-            'trace' => $traceJson
-        ]);
-    }
-
-    public function logInfo(Request $request, $data)
-    {
-        $headers = json_encode($request->header());
-        $body = json_encode($request->all());
-        Log::info([
-            'request-id' => $request->header('x-request-id'),
-            'message' => "[SUCCESS] ",
-            'data' => $data,
-            'header' => $headers,
-            'body' => $body,
-        ]);
-    }
-
-    public function is_null(mixed $val){
-        return !$val;
-    }
 }

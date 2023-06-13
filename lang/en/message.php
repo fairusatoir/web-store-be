f7d884ae0c48f7c9453a5b595e47188d75305887
IDCB.Fairus Zuhair Azizy Atoir <IDCB.FAIRUSZ@xl.co.id>
2023-06-13 13:04:35 +0700

    feat: locale en

---
+++ lang/en/message.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'wrong' => 'Something wrong, Try again!',
+    'available' => 'No data available',
+    'success' => [
+        'create' => 'Create Data Success',
+        'update' => 'Update Data Success',
+        'delete' => 'Delete Data Success',
+    ],
+    'title' => [
+        'product' => [
+            'index' => 'List Product',
+            'create' => 'Create Product',
+            'update' => 'Edit Product',
+        ],
+    ],
+];

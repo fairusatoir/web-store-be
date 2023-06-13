4691600c0661aa4732787449d53c7334ac8504a8
IDCB.Fairus Zuhair Azizy Atoir <IDCB.FAIRUSZ@xl.co.id>
2023-06-13 12:43:16 +0700

    fix: change to route group

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use App\Helpers\RouteHelper;
-use Mockery\Generator\Method;
+use Illuminate\Routing\RouteGroup;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\ProductGalleryController;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
@@ -23,68 +26,20 @@ Route::get('/', [DashboardController::class, 'index'])->name('dashboard')->middl
 // @phpstan-ignore-next-line
 Auth::routes(['register' => false]);
 
-$pages = [
-    [
-        'name' => 'Product',
-        'only' => ['index', 'create', 'store', 'edit', 'update', 'destroy'],
-        'nested' => [
-            [
-                'name' => 'gallery',
-                'method' => 'get',
-                'params' => 'product'
-            ]
-        ]
-    ],
-    [
-        'name' => 'Product Gallery',
-        'only' => ['index', 'create', 'store', 'destroy'],
-    ],
-    [
-        'name' => 'Transaction',
-        'only' => ['index', 'show', 'edit', 'update', 'destroy'],
-        'nested' => [
-            [
-                'name' => 'set status',
-                'method' => 'get',
-                'params' => 'transaction'
-            ]
-        ]
-    ],
-];
+Route::middleware(['auth'])->group(function () {
 
-foreach ($pages as $page) {
+    Route::resource('products', ProductController::class)
+        ->except(['show']);
 
-    $route = new RouteHelper($page);
+    Route::get('products/{product}/galleries', [ProductController::class, 'gallery'])
+        ->name('products.galleries');
 
-    Route::resource(
-        $route->getUri(),
-        $route->getController()
-    )->only($page['only'] ?? null)->middleware('auth');
+    Route::resource('product-galleries', ProductGalleryController::class)
+        ->except(['show', 'edit', 'update']);
 
-    if ($route->nestedExist()) {
-        foreach ($route->getSubroute() as $subroute) {
-            switch ($subroute['method']) {
-                case 'get':
-                    Route::get(
-                        $route->getSubRouteUri($subroute),
-                        $route->getSubController($subroute)
-                    )->name($route->getNameSubroute($subroute))->middleware('auth');
-                    break;
-                case 'post':
-                    // Kode yang akan dijalankan jika $subroute['method'] sama dengan 'post'
-                    // ...
-                    break;
-                case 'put':
-                    // Kode yang akan dijalankan jika $subroute['method'] sama dengan 'put'
-                    // ...
-                    break;
-                case 'delete':
-                    // Kode yang akan dijalankan jika $subroute['method'] sama dengan 'delete'
-                    // ...
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-}
+    Route::resource('transactions', TransactionController::class)
+        ->except(['create', 'store']);
+
+    Route::get('transactions/{product}/set-status', [TransactionController::class, 'setStatus'])
+        ->name('transactions.status');
+});
